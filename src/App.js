8c57e2ba8bead17861e58d23cbb72ce1ccdb878c
Edher Jiménez <edher.jimenez@gmail.com>
2021-09-08 17:19:36 -0500

    choroplet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
-@@ -10,6 +10,7 @@ class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-@@ -29,4 +30,4 @@ class App extends Component {
-  }
-}
+import React from 'react';
+import {quantile} from 'd3-array';
+import {scaleLog} from 'd3-scale';
+import {interpolateHcl} from 'd3-interpolate';
+import {geoPath} from 'd3-geo';
+import topojson from 'topojson';
 
-export default withAuthenticator(App, true);
-export default withAuthenticator(App);
+import us from '../data/us-albers.json';
+
+const width = 960;
+const height = 500;
+
+const color = scaleLog()
+  .range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
+  .interpolate(interpolateHcl);
+
+const path = geoPath()
+  .projection(null);
+
+export default function Population() {
+  const counties = topojson.feature(us, us.objects.counties).features;
+
+  const densities = counties
+    // TODO(joel): make this not side effect
+    .map(d => { return d.properties.density = d.properties.pop / path.area(d); })
+    .sort((a, b) => a - b);
+
+  color.domain([quantile(densities, .01), quantile(densities, .99)]);
+
+  const paths = counties.map(county => (
+    <path
+      key={county.id}
+      fill={color(county.properties.density)}
+      d={path(county)}
+    />
+  ));
+
+  return (
+    <svg width={width} height={height}>
+      <g className="counties">
+        {paths}
+      </g>
+    </svg>
+  );
+}
